363d0d8dac30127139b7264cc38d3eb128b8bffa
Ashish Ranjan Samal <153530109+1MRGD@users.noreply.github.com>
2025-09-16 19:10:25 +0000

    conversion opertion

--- 01_Basics_js/03_conversionOperation.js
+++ 01_Basics_js/03_conversionOperation.js
@@ -2,24 +2,24 @@ let Score = 33
 let Score1 = "33abc"
 let Score2 =  null
 
-console.log(typeof Score);//number
-console.log(typeof (Score)); //number
+// console.log(typeof Score);//number
+// console.log(typeof (Score)); //number
 
 
-let valueInNumber = Number(Score)
-console.log(typeof Score);//number
-console.log(typeof valueInNumber);  //number
-console.log(valueInNumber) //33
+// let valueInNumber = Number(Score)
+// console.log(typeof Score);//number
+// console.log(typeof valueInNumber);  //number
+// console.log(valueInNumber) //33
 
-let valueInNumber_1 = Number(Score1)
-console.log(typeof Score1);//string
-console.log(typeof valueInNumber_1); //number
-console.log(valueInNumber_1) //NaN
+// let valueInNumber_1 = Number(Score1)
+// console.log(typeof Score1);//string
+// console.log(typeof valueInNumber_1); //number
+// console.log(valueInNumber_1) //NaN
 
-let valueInNumber_2 = Number(Score2)
-console.log(typeof Score2);//object
-console.log(typeof valueInNumber_2); //number
-console.log(valueInNumber_2) //0
+// let valueInNumber_2 = Number(Score2)
+// console.log(typeof Score2);//object
+// console.log(typeof valueInNumber_2); //number
+// console.log(valueInNumber_2) //0
 
 
 //"33" => 33
@@ -41,14 +41,57 @@ console.log(valueInNumber_2) //0
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber); // 33
-console.log(typeof(stringNumber)); // string
+// console.log(stringNumber); // 33
+// console.log(typeof(stringNumber)); // string
+
+
+/*===========================Operation=========================*/
+let value = 3;
+let negValue = -value;
+//console.log(negValue)
 
 
 
+// console.log(2+2) //4
+// console.log(2-2) //0
+// console.log(2*2) //4
+// console.log(2**3)//8
+// console.log(2/3) //0.666666666666666666666
+// console.log(2%3) //2
+
+let str1 = "Hello"
+let str2 = " GD"
+
+
+// let str3 = str1+str2
+// console.log(str3) //Hello GD
+
+
+// console.log("1"+2);//12
+// console.log(1+"2");//12
+// console.log("1"+"2");//12
+// console.log("1"+2+2);//122
+// console.log(1+2+"2");//32 
 
 
 
+// console.log((3+4) * 5 % 3); //2
+
+// console.log(+true); 
+// console.log(+"");
 
 
+let num1, num2, num3
+ 2+2
+ 
+ num1 = num2 = num3 = 2+2
+ 
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)//101
 
+let gameCounter_1 = 100
+++gameCounter_1;
+console.log(gameCounter_1)//101
+
+//Link To Study
